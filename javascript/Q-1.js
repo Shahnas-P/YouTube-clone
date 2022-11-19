9c2b82e4040d11e1060b23af51c579030daa806f
Shahnas <shahanasp9@gmail.com>
2022-11-19 13:32:41 +0530

    header and sidebar finished

---
+++ javascript/Q-1.js
@@ -0,0 +1,9 @@
+// Accept a char input from the user and display it on the console.
+function getUsername(){
+    var Username =document.getElementById("name").value;
+    var msg= "Hey "+Username + " How are You?"
+    document.getElementById("print").innerHTML=msg;
+}
+
+
+
